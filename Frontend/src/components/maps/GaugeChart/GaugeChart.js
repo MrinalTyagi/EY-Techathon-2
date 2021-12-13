c74c0c760c4fd0397fc2251bd3cac4b5da07eb21
AAVAIG2001 <aavaigmalhotra2001@gmail.com>
2021-12-13 16:24:10 +0530

    add: main page

--- Frontend/src/components/maps/GaugeChart/GaugeChart.js
+++ Frontend/src/components/maps/GaugeChart/GaugeChart.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Chart } from 'react-google-charts';
 
-function GaugeChart() {
+function GaugeChart({ aqiParams }) {
+  // const [selectedYear, setSelectedYear] = useState(2015);
+
+  const [selectedAqiParam, setSelectedAqiParam] = useState('SO2');
+
   return (
     <div
       style={{
@@ -11,26 +15,41 @@ function GaugeChart() {
         flexDirection: 'column',
       }}
     >
-      <Chart
-        width={'100%'}
-        height={'100%'}
-        chartType='Gauge'
-        loader={<div>Loading Chart</div>}
-        data={[
-          ['Label', 'Value'],
-          ['Memory', 55],
-          //   ['CPU', comp.state.cpu],
-          //   ['Network', comp.state.network],
-        ]}
-        options={{
-          redFrom: 90,
-          redTo: 100,
-          yellowFrom: 75,
-          yellowTo: 90,
-          minorTicks: 5,
-        }}
-        rootProps={{ 'data-testid': '1' }}
-      />
+      {aqiParams.length !== 0 && (
+        <>
+          <div className='aqi-param-selector'>
+            <select
+              value={selectedAqiParam}
+              onChange={(e) => setSelectedAqiParam(e.target.value)}
+            >
+              <option value='SO2'>SO2</option>
+              <option value='NO2'>NO2</option>
+              <option value='RSPM'>RSPM</option>
+              <option value='SPM'>SPM</option>
+              <option value='PM25'>PM25</option>
+            </select>
+          </div>
+
+          <Chart
+            width={'100%'}
+            height={'100%'}
+            chartType='Gauge'
+            loader={<div>Loading Chart</div>}
+            data={[
+              ['AQI Param', '%Value'],
+              [selectedAqiParam, aqiParams[selectedAqiParam] / 100000],
+            ]}
+            options={{
+              redFrom: 90,
+              redTo: 100,
+              yellowFrom: 75,
+              yellowTo: 90,
+              minorTicks: 5,
+            }}
+            rootProps={{ 'data-testid': '1' }}
+          />
+        </>
+      )}
     </div>
   );
 }
